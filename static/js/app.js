92d6ede0f114a736e2edb651e30b85a3086e97e3
Jaime Rodriguez <rodriguez.jaime.a@gmail.com>
2019-08-19 21:27:36 -0700

    Date filter complete

--- static/js/app.js
+++ static/js/app.js
@@ -6,23 +6,50 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 
-    // Select the input element and get the raw HTML node
-    var date_selected = d3.select("#datetime").property("value");
-    // console.log(date_selected);
-//     console.log(ufos);
+// Select the input element and get the raw HTML node
+var date_selected = d3.select("#datetime").property("value");
 
-  
 var filteredufos = ufos.filter(ufo => ufo.datetime === date_selected);
 
 console.log(filteredufos);
 
-var ufo_arr = [];
-for (var key in filteredufos) {
-    if (filteredufos.hasOwnProperty(key)) {
-        ufo_arr.push( [key,filteredufos[key]]);
-        
-    }
-}
+// Extract date
+var ufo_date = filteredufos.map(function(ufo_set){
+    return ufo_set.datetime;
+})
+console.log(ufo_date);
+
+// Extract city
+var ufo_city = filteredufos.map(function(ufo_set){
+    return ufo_set.city;
+})
+console.log(ufo_city);
+
+var ufo_state = filteredufos.map(function(ufo_set){
+    return ufo_set.state;
+})
+console.log(ufo_state);
+
+var ufo_country = filteredufos.map(function(ufo_set){
+    return ufo_set.country;
+})
+console.log(ufo_country);
+
+var ufo_shape = filteredufos.map(function(ufo_set){
+    return ufo_set.shape;
+})
+console.log(ufo_shape);
+
+var ufo_duration = filteredufos.map(function(ufo_set){
+    return ufo_set.durationMinutes;
+})
+console.log(ufo_duration);
+
+var ufo_comments = filteredufos.map(function(ufo_set){
+    return ufo_set.comments;
+})
+console.log(ufo_comments);
+
 
 // Use D3 to select the table
 var table = d3.select("#ufo-table");
@@ -34,19 +61,19 @@ table.attr("class", "table table-striped");
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
-// Append one table row `tr` to the table body
-var row = tbody.append("tr");
 
-row.append("td").text(ufo_arr[0]);
+for (var i = 0; i < filteredufos.length; i++) {
+    // Append one table row `tr` to the table body
+    var row = tbody.append("tr");    
 
-// for (let [key, value] of Object.entries(filteredufos[0])) {
-
-//     console.log(`${key}: ${value}`);
-// }
-
-// filteredufos.forEach(function(element){
-//     console.log(element);
-//     row.append("td").text(element);
-// })
+    row.append("td").text(ufo_date[i]);
+    row.append("td").text(ufo_city[i]);
+    row.append("td").text(ufo_state[i]);
+    row.append("td").text(ufo_country[i]);
+    row.append("td").text(ufo_shape[i]);
+    row.append("td").text(ufo_duration[i]);
+    row.append("td").text(ufo_comments[i]);
+}
+console.log(length.filteredufos);
 
 })
